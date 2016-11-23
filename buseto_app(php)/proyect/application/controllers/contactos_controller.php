9ecc5ff7b524fabe4b96ae38011c8520d57afcbd
Jhon Edison Vivas Moreno <jhon.vivas@produccion.siesa.com>
2016-11-23 18:28:05 -0500

    1- inicio de proyecto

---
+++ buseto_app(php)/proyect/application/controllers/contactos_controller.php
@@ -0,0 +1,14 @@
+<?php
+
+class contactos_controller extends CI_Controller {
+
+	public function index(){
+		echo "Este es el contactos controller";
+	}
+
+	public function agregar(){
+		echo "Este es agregar";
+	}
+}
+
+?>
