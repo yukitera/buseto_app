b789128453be91351cc7e13d1f07ee8a9c97114f
Jhon Edison Vivas Moreno <jhon.vivas@produccion.siesa.com>
2016-11-28 17:03:10 -0500

    28-11-2016 (correccion)

--- buseto_app(php)/proyect/application/controllers/home.php
+++ buseto_app(php)/proyect/application/controllers/home.php
@@ -2,10 +2,28 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class home extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+		$this -> load -> model('model_country');
+	}
+
 	public function index()
 	{
 		$this -> load -> view("plantilla/header");
 		$this -> load -> view("home/index");
+		$this -> load -> view("plantilla/form");
 		$this -> load -> view("plantilla/footer");
 	}
+
+	public function insertCountry()
+	{
+		$data = array(
+			'country_name' => $this -> input -> POST('name'),
+			'country_flag' => $this -> input -> POST('flag')
+		);
+		
+		$this -> model_country ->  insert($data);
+	}
+	
 }
